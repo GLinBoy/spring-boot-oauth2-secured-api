53b559a5fd46bbe1d351c9e2600cb74498fb4f2f
GLinBoy <hojjatabedie@gmail.com>
2023-07-31 19:14:47 +0200

    👌 IMPROVE: Added whitelist URL

--- src/main/kotlin/com/glinboy/test/springboot/book/config/SecurityConfig.kt
+++ src/main/kotlin/com/glinboy/test/springboot/book/config/SecurityConfig.kt
@@ -6,17 +6,34 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    private val AUTH_WHITELIST = arrayOf(
+        "/",
+        "/swagger-ui.html",
+        "/v3/api-docs/**",
+        "/swagger-ui/**",
+        // other public endpoints of your API may be appended to this array
+        "/h2-console/**"
+    )
+
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
             .cors { it.disable() }
             .csrf { it.disable() }
-            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(
+                        *AUTH_WHITELIST.map { AntPathRequestMatcher(it) }.toTypedArray()
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            }
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
             .build()
     }
