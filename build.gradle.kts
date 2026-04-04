3cd643fa85c1dc1d37af3d6652028c1f4df8f9a4
GLinBoy <glinboy@live.com>
2026-04-04 12:00:50 +0200

    👌 IMPROVE: Upgrade Spring Boot to 4.0.5
    resolved #123

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.2"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.2.21"
     id ("com.github.ben-manes.versions") version "0.53.0"
