71023495a175efc77a532a738720af944604b1c2
GLinBoy <glinboy@live.com>
2026-06-22 10:30:16 +0200

    👌 IMPROVE: Upgrade Kotlin to 2.3.21
    closed #133

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.3.21"
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.jpa") version "2.3.21"
     id ("com.github.ben-manes.versions") version "0.53.0"
     id ("org.ec4j.editorconfig") version "0.1.0"
 }
